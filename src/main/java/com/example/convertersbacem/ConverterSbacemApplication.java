216d16294f66e309dd8a82b3c90154d6af826b13
marlonfonseca <marlon.fonsecadeoliveira@gmail.com>
2023-05-01 20:42:52 -0300

    Commit inicial

--- src/main/java/com/example/convertersbacem/ConverterSbacemApplication.java
+++ src/main/java/com/example/convertersbacem/ConverterSbacemApplication.java
@@ -40,48 +40,48 @@ public class ConverterSbacemApplication {
                         //REGISTRO – ‘2’ – DADOS CONSOLIDADOS DOS TITULARES PROCESSADOS
                         if (TP_REG.equals("2")) {
 
-                            String CD_ECAD = line.substring(2, 13);
-                            String CD_CAE = line.substring(14, 24);
-                            String CPF_CGC = line.substring(25, 38);
-                            String CD_IFPI = line.substring(39, 41);
-                            String NMTIT = line.substring(42, 86);
-                            String PSEUDO = line.substring(87, 131);
-                            String NR_RECB = line.substring(132, 144);
-                            String VL_T_DIST = line.substring(145, 166);
-                            String VL_T_INC = line.substring(167, 188);
-                            String VL_T_RET = line.substring(189, 210);
-                            String VL_T_PEND = line.substring(211, 232);
-                            String VL_T_PARM = line.substring(233, 254);
-                            String VL_T_DESC = line.substring(255, 276);
-                            String VL_T_CRED = line.substring(277, 298);
-                            String VL_T_REC = line.substring(299, 320);
-                            String RAD_IFPI = line.substring(321, 322);
-                            String NOM_PLANO = line.substring(323, 372);
-                            String IND_PGSUBEDITPROD = line.substring(373, 373);
-                            String TIP_AMOSTRA = line.substring(374, 374);
-                            String FILLER = line.substring(375, 400);
+                            String CD_ECAD = line.substring(1, 12);
+                            String CD_CAE = line.substring(13, 23);
+                            String CPF_CGC = line.substring(24, 37);
+                            String CD_IFPI = line.substring(38, 40);
+                            String NMTIT = line.substring(41, 85);
+                            String PSEUDO = line.substring(86, 130);
+                            String NR_RECB = line.substring(131, 143);
+                            String VL_T_DIST = line.substring(144, 165);
+                            String VL_T_INC = line.substring(166, 187);
+                            String VL_T_RET = line.substring(188, 209);
+                            String VL_T_PEND = line.substring(210, 231);
+                            String VL_T_PARM = line.substring(232, 253);
+                            String VL_T_DESC = line.substring(254, 275);
+                            String VL_T_CRED = line.substring(276, 297);
+                            String VL_T_REC = line.substring(298, 319);
+                            String RAD_IFPI = line.substring(320, 321);
+                            String NOM_PLANO = line.substring(322, 371);
+                            String IND_PGSUBEDITPROD = line.substring(372, 372);
+                            String TIP_AMOSTRA = line.substring(373, 373);
+                            String FILLER = line.substring(374, 399);
 
                         }
                         //REGISTRO – ‘3’ – DADOS POR RUBRICA / CATEGORIA DOS TITULARES PROCESSADOS
                         if (TP_REG.equals("3")){
 
-                            String NR_RECB = line.substring(2, 14);
-                            String CD_RUBR = line.substring(15, 20);
-                            String DC_RUBR = line.substring(21, 70);
-                            String CD_CAT = line.substring(71, 72);
-                            String VL_DIST = line.substring(73, 94);
-                            String VL_INC = line.substring(95, 116);
-                            String VL_RET = line.substring(117, 138);
-                            String VL_PEND = line.substring(139, 160);
-                            String VL_PARM = line.substring(161, 182);
-                            String VL_DESC = line.substring(183, 204);
-                            String VL_CRED = line.substring(205, 226);
-                            String VL_TOT = line.substring(227, 248);
-                            String IND_MANUAL = line.substring(249, 249);
-                            String NOM_PLANO = line.substring(250, 299);
-                            String IND_PGSUBEDITPROD = line.substring(300, 300);
-                            String TIP_AMOSTRA = line.substring(301, 301);
-                            String FILLER = line.substring(302, 350);
+                            String NR_RECB = line.substring(1, 13);
+                            String CD_RUBR = line.substring(14, 19);
+                            String DC_RUBR = line.substring(20, 69);
+                            String CD_CAT = line.substring(70, 71);
+                            String VL_DIST = line.substring(72, 93);
+                            String VL_INC = line.substring(94, 115);
+                            String VL_RET = line.substring(116, 137);
+                            String VL_PEND = line.substring(138, 159);
+                            String VL_PARM = line.substring(160, 181);
+                            String VL_DESC = line.substring(182, 203);
+                            String VL_CRED = line.substring(204, 225);
+                            String VL_TOT = line.substring(226, 247);
+                            String IND_MANUAL = line.substring(248, 248);
+                            String NOM_PLANO = line.substring(249, 298);
+                            String IND_PGSUBEDITPROD = line.substring(299, 299);
+                            String TIP_AMOSTRA = line.substring(300, 300);
+                            String FILLER = line.substring(301, 349);
 
 
                         }
@@ -89,18 +89,18 @@ public class ConverterSbacemApplication {
                         //REGISTRO – ‘4’ – IDENTIFICAÇÃO DOS AJUSTES PROCESSADOS
                         if (TP_REG.equals("4")){
 
-                            String NR_RECB = line.substring(2, 14);
-                            String CD_RUBR = line.substring(15, 20);
-                            String DC_RUBR = line.substring(21, 70);
-                            String CD_CAT = line.substring(71, 72);
-                            String TP_AJUSTE = line.substring(73, 117);
-                            String CRED_DEPT = line.substring(118, 118);
-                            String VL_AJUSTE = line.substring(119, 140);
+                            String NR_RECB = line.substring(1, 13);
+                            String CD_RUBR = line.substring(14, 19);
+                            String DC_RUBR = line.substring(20, 69);
+                            String CD_CAT = line.substring(70, 71);
+                            String TP_AJUSTE = line.substring(72, 116);
+                            String CRED_DEPT = line.substring(117, 117);
+                            String VL_AJUSTE = line.substring(118, 139);
 
-                            String NOM_PLANO = line.substring(141, 190);
-                            String IND_PGSUBEDITPROD = line.substring(191, 191);
-                            String TIP_AMOSTRA = line.substring(192, 192);
-                            String FILLER = line.substring(193, 300);
+                            String NOM_PLANO = line.substring(140, 189);
+                            String IND_PGSUBEDITPROD = line.substring(190, 190);
+                            String TIP_AMOSTRA = line.substring(191, 191);
+                            String FILLER = line.substring(192, 299);
 
 
                         }
@@ -122,12 +122,19 @@ public class ConverterSbacemApplication {
             XSSFSheet sheet = workbook.createSheet("Planilha 1");
             int rowIndex = 0;
             for (String[] rowData : data) {
+                String TP_REG = rowData[0].substring(0, 1);
+               // if(TP_REG.equals("2") || TP_REG.equals("3") || TP_REG.equals("4") ) {
+
                 XSSFRow row = sheet.createRow(rowIndex++);
                 int cellIndex = 0;
-                for (String cellData : rowData) {
-                    XSSFCell cell = row.createCell(cellIndex++);
-                    cell.setCellValue(cellData);
-                }
+
+
+                    for (String cellData : rowData) {
+                        XSSFCell cell = row.createCell(cellIndex++);
+                        cell.setCellValue(cellData);
+
+                   }
+               // }
             }
 
             File fileOut = new File("C:\\Users\\marlo\\OneDrive\\backup_sbacem\\Sbacem\\CONVERSOR MODELO UBC TXT XLS\\arquivo_ubc.xlsx");
@@ -149,5 +156,6 @@ public class ConverterSbacemApplication {
 
         SpringApplication.run(ConverterSbacemApplication.class, args);
     }
+
 
 }
